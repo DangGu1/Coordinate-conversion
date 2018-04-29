30dfa51ece13ea1133e7291dfcc541edb9ff1214
Mushano <az435074026@qq.com>
2018-04-29 22:51:12 +0800

    添加进去

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace DataGridViewTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonAdd1 = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear1 = new System.Windows.Forms.Button();
+            this.buttonCal1 = new System.Windows.Forms.Button();
+            this.radioButton60 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.ColumnLa,
+            this.ColumnLong,
+            this.ColumnH});
+            this.dataGridView1.Location = new System.Drawing.Point(46, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(756, 196);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "X";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Y";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Z";
+            this.Column3.Name = "Column3";
+            // 
+            // ColumnLa
+            // 
+            this.ColumnLa.HeaderText = "Latitude";
+            this.ColumnLa.Name = "ColumnLa";
+            // 
+            // ColumnLong
+            // 
+            this.ColumnLong.HeaderText = "Longtitude";
+            this.ColumnLong.Name = "ColumnLong";
+            // 
+            // ColumnH
+            // 
+            this.ColumnH.HeaderText = "Height";
+            this.ColumnH.Name = "ColumnH";
+            // 
+            // buttonAdd1
+            // 
+            this.buttonAdd1.Location = new System.Drawing.Point(46, 41);
+            this.buttonAdd1.Name = "buttonAdd1";
+            this.buttonAdd1.Size = new System.Drawing.Size(76, 29);
+            this.buttonAdd1.TabIndex = 3;
+            this.buttonAdd1.Text = "添加（&A）";
+            this.buttonAdd1.UseVisualStyleBackColor = true;
+            this.buttonAdd1.Click += new System.EventHandler(this.buttonAdd1_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(140, 41);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(76, 29);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "删除（&D）";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonClear1
+            // 
+            this.buttonClear1.Location = new System.Drawing.Point(241, 41);
+            this.buttonClear1.Name = "buttonClear1";
+            this.buttonClear1.Size = new System.Drawing.Size(76, 29);
+            this.buttonClear1.TabIndex = 5;
+            this.buttonClear1.Text = "清空（&C）";
+            this.buttonClear1.UseVisualStyleBackColor = true;
+            this.buttonClear1.Click += new System.EventHandler(this.buttonClear1_Click);
+            // 
+            // buttonCal1
+            // 
+            this.buttonCal1.Location = new System.Drawing.Point(693, 32);
+            this.buttonCal1.Name = "buttonCal1";
+            this.buttonCal1.Size = new System.Drawing.Size(109, 47);
+            this.buttonCal1.TabIndex = 11;
+            this.buttonCal1.Text = "<-计算";
+            this.buttonCal1.UseVisualStyleBackColor = true;
+            this.buttonCal1.Click += new System.EventHandler(this.buttonCal1_Click);
+            // 
+            // radioButton60
+            // 
+            this.radioButton60.AutoSize = true;
+            this.radioButton60.Location = new System.Drawing.Point(40, 24);
+            this.radioButton60.Name = "radioButton60";
+            this.radioButton60.Size = new System.Drawing.Size(88, 19);
+            this.radioButton60.TabIndex = 12;
+            this.radioButton60.TabStop = true;
+            this.radioButton60.Text = "六十进制";
+            this.radioButton60.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(40, 49);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(73, 19);
+            this.radioButton10.TabIndex = 13;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "十进制";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton10);
+            this.groupBox1.Controls.Add(this.radioButton60);
+            this.groupBox1.Location = new System.Drawing.Point(454, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 77);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "格式选择";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(822, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 362);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonCal1);
+            this.Controls.Add(this.buttonClear1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "大地坐标转空间坐标";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAdd1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClear1;
+        private System.Windows.Forms.Button buttonCal1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnLa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnLong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnH;
+        private System.Windows.Forms.RadioButton radioButton60;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataGridViewTask
+{
+    public partial class Form1 : Form
+    {
+        double N;
+        const double a = 6378137;
+        const double b = 6356752.31414;
+        double e = Math.Sqrt(Math.Pow(a,2)-Math.Pow(b,2))/a;
+        bool SelectRadioJudgement;//用于判断是否选择了格式
+        public Form1()
+        {
+            InitializeComponent();
+            AddRow();
+            SetFormat();
+        }
+        //设置DataGridView的默认格式
+        private void SetFormat()
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;//第一列不再显示
+            dataGridView1.AllowUserToResizeColumns = false;//用户不可以自己调整列大小
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.ReadOnly = false;
+        }
+        //添加行
+        private int AddRow()
+        {
+                int index = dataGridView1.Rows.Add();
+                return index;
+
+        }
+        //大地坐标系的结构体
+        struct GC
+        {
+            public double latitude;
+            public double longtitude;
+            public double height;
+        }
+        //空间直角坐标系
+        struct SpaceRC
+        {
+            public double x;
+            public double y;
+            public double z;
+
+        }
+        private void buttonAdd1_Click(object sender, EventArgs e)
+        {
+            AddRow();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount != 0)
+            {
+                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+            }
+            else
+            {
+                MessageBox.Show("已经没有可以删除的行了！");
+            }
+        }
+
+        private void buttonClear1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var temp = dataGridView1.Rows[0].Cells;
+            temp[3].Value=30.3155;
+            temp[4].Value = 114.2127;
+            temp[5].Value = 134.6;
+        }
+        //角度化弧度  34°25′23″ = 34.2523以及格式识别
+        private double ToRadian(double angle)
+        {
+            double degree;
+            double minute;
+            double second;
+            double ret;
+            if (radioButton10.Checked==true)//判断点击了哪个按钮
+            {
+                degree = (int)(angle);
+                minute = (int)((angle - degree) * 100);
+                second = (int)((angle - degree - minute / 100) * 10000);
+                ret = (degree + minute / 60 + second / 3600) / 180 * Math.PI;
+                SelectRadioJudgement = true;
+                return ret;
+            }
+            else if (radioButton60.Checked==true)
+            {
+                ret = angle * Math.PI / 180;
+                SelectRadioJudgement = true;
+                return ret;
+            }
+            else
+            {
+                SelectRadioJudgement = false;
+                return 0;
+            }
+
+        }
+        //把输入的数据传入到变量中
+        private void AssignGC1(ref GC gc)
+        {
+            var temp = dataGridView1.CurrentRow.Cells;
+            gc.latitude = ToRadian(double.Parse(temp[3].Value.ToString()));
+            gc.longtitude = ToRadian(double.Parse(temp[4].Value.ToString()));
+            gc.height = double.Parse(temp[5].Value.ToString());
+            
+        }
+        //计算X坐标
+        private double calculateX(GC temp)
+        {
+            double B = temp.latitude;
+            double L = temp.longtitude;
+            double H = temp.height;
+            N =  a / Math.Sqrt(1-Math.Pow(e,2)*Math.Sin(B));
+            return(N + H) * Math.Cos(B) * Math.Cos(L);//返回N
+
+        }
+        //计算Y坐标
+        private double calculateY(GC temp)
+        {
+            double B = temp.latitude;
+            double L = temp.longtitude;
+            double H = temp.height;
+            return (N + H) * Math.Cos(B) * Math.Sin(L);
+        }
+        //计算Z坐标
+        private double calculateZ(GC temp)
+        {
+            double B = temp.latitude;
+            double L = temp.longtitude;
+            double H = temp.height;
+            return (N * (1.0 - FlatRate * (2 - FlatRate)) + H) * Math.Sin(B);
+        }
+        private void buttonCal1_Click(object sender, EventArgs e)
+        {
+            if (emptyCheck()&&inputCheck())
+            {
+                var temp = dataGridView1.CurrentRow.Cells;
+                GC coordinateGC1 = new GC();
+                AssignGC1(ref coordinateGC1);
+                if (SelectRadioJudgement)//是否
+                {
+                    temp[0].Value = calculateX(coordinateGC1).ToString("F5");
+                    temp[1].Value = calculateY(coordinateGC1).ToString("F5");
+                    temp[2].Value = calculateZ(coordinateGC1).ToString("F5");
+                }
+                else
+                {
+                    MessageBox.Show("请先选择格式！");
+                }
+            }
+
+        }
+
+        //计算快捷键 Enter
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonCal1_Click(sender, e);
+            } 
+        }
+        private bool inputCheck()
+        {
+            int j = 0;
+            double temp;
+            for (int i = 3; i < dataGridView1.ColumnCount; i++)
+            {
+                if (double.TryParse(dataGridView1.CurrentRow.Cells[i].Value.ToString(), out temp))
+                {
+                    j++;
+                }
+                else
+                {
+                    MessageBox.Show("第" + (i +1)+ "列的数据格式有误！");
+                    break;
+                }
+            }
+            if (j == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //对单元格内的数据进行非空检查
+        private bool emptyCheck()
+        {
+            int i = 0;
+            var tag = dataGridView1.CurrentRow.Cells;
+            for (int j = 3; j < dataGridView1.ColumnCount ; j++)
+            {
+                if (tag[j].Value == null)
+                {
+                    MessageBox.Show("你的第" + (j +1)+ "格为空，请输入数据");
+                    break;
+                }
+                i++;
+            }
+            if (i == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
